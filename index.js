0c658722617205805df28c0292ea12e5176c07d0
Ghost Hackz <ghosthackz@pginteractive.ml>
2017-06-10 20:49:35 +0545

    Create index.js

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const NAME_ACTION = 'make_name';
 const COLOR_ARGUMENT = 'color';
 const NUMBER_ARGUMENT = 'number';
 
-const app = new App({request, response});
-console.log('Request headers: ' + JSON.stringify(request.headers));
-console.log('Request body: ' + JSON.stringify(request.body));
+exports.mainThread = (request, response) => {
+	const app = new App({request, response});
+	console.log('Request headers: ' + JSON.stringify(request.headers));
+	console.log('Request body: ' + JSON.stringify(request.body));
 
-function makeName(app) {
-	let number = app.getArgument(NUMBER_ARGUMENT);
-	let color = app.getArgument(COLOR_AURGUMENT);
-	app.tell('Alright your silly name is ' + color + ' ' + number + '! I hope you like it. See you next time.');
-}
+	function makeName(app) {
+		let number = app.getArgument(NUMBER_ARGUMENT);
+		let color = app.getArgument(COLOR_AURGUMENT);
+		app.tell('Alright your silly name is ' + color + ' ' + number + '! I hope you like it. See you next time.');
+	}
 
-let actionMap = new Map();
-actionMap.set(NAME_ACTION, makeName);
+	let actionMap = new Map();
+	actionMap.set(NAME_ACTION, makeName);
 
-app.handleRequest(actionMap);
+	app.handleRequest(actionMap);
+};
