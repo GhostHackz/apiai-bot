8bb849443c641b8e97017692faeeb84b0016f153
Ghost Hackz <ghosthackz@pginteractive.ml>
2017-06-12 08:27:33 +0545

    Create index.js

--- index.js
+++ index.js
@@ -15,25 +15,34 @@ service.use(parser.urlencoded({
 
 service.use(parser.json());
 
+const CAT_SOUND = 'https://actions.google.com/sounds/v1/animals/cat_purr_close.ogg';
+const DOG_SOUND = 'https://actions.google.com/sounds/v1/animals/dog_growling.ogg';
+const MOUSE_SOUND = 'https://actions.google.com/sounds/v1/animals/mouse_squeaking.ogg';
+
 service.post('/ai-management-system', function(request, response) {
 	const app = new App({request, response});
 	console.log('Request headers: ' + JSON.stringify(request.headers));
 	console.log('Request body: ' + JSON.stringify(request.body));
 	
-	function aiGreet(app) {
-		app.ask('Hey! How are you?');
+	function catSound(app) {
+		let catSpeech = 'Alright, here\'s a cat sound.<audio src="${CAT_SOUND}"></audio>';
+		app.ask('<speak>${catSpeech}</speak>');
 	}
 	
-	function aiDate(app) {
-		var date = date-time.create();
-		date.format('Y-m-d H:M:S');
-		
-		app.tell(new Date(date.now()));
+	function dogSound(app) {
+		let dogSpeech = 'Alright, here\'s a dog sound.<audio src="${DOG_SOUND}"></audio>';
+		app.ask('<speak>${dogSpeech}</speak>');
+	}
+	
+	function mouseSound(app) {
+		let mouseSpeech = 'Alright, here\'s a mouse sound.<audio src="${MOUSE_SOUND}"></audio>';
+		app.ask('<speak>${mouseSpeech}</speak>');
 	}
 	
 	const actionMap = new Map();
-	actionMap.set('ai.greet', aiGreet);
-	actionMap.set('ai.date', aiDate);
+	actionMap.set('ai.cat-sound', catSound);
+	actionMap.set('ai.dog-sound', dogSound);
+	actionMap.set('ai.mouse-sound', mouseSound);
 	
 	app.handleRequest(actionMap);
 });
