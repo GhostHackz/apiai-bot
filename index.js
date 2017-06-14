5f3a68ebaf29991a1ae5663c174e5a70924f4be7
Ghost Hackz <ghosthackz@pginteractive.ml>
2017-06-14 08:44:56 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const TwitterPackage = require('twitter');
+const express = require('express');
+const parser = require('body-parser');
+
+const service = express();
+
+service.use(parser.urlencoded({
+	extended: true
+}));
+
+service.use(parser.json());
+
+const keys = require('./keys');
+
+const Twitter = new TwitterPackage(keys);
+
+service.post('/twitter-webhook', function(request, response) {
+	const tweet = tweet-text;
+	response.send(true);
+	
+	Twitter.post('statuses/update', {status: tweet}, function(error, tweet, response) {
+		if (error) {
+			console.log(error);
+		}
+		console.log(tweet.text);
+		console.log(response);
+	});
+});
+
+service.listen((process.env.PORT || 5000), function() {
+	console.log('Server is up and listening!');
+});
